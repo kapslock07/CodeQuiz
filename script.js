a6c132d99bd47550854f47816200bd788938690a
Ryan Kaplan <ryankaplan7@gmail.com>
2020-01-17 18:29:09 -0500

    added highschore link, timer placeholder, directions, and question 1 placeholder

---
+++ script.js
@@ -0,0 +1,3 @@
+var clickButton = $("#startButton").on("click", function () {
+    var firstQuestion = $("#mainSection").html("<p>This is Question 1</p>" + "<div><button>Answer 1</button></div>" + "<div><button>Answer 2</button></div>" + "<div><button>Answer 3</button></div>" + "<div><button>Answer 4</button></div>");
+});
